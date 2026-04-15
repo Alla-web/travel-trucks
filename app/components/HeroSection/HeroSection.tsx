2ddd8fc22d3ae8d926749387498a669b7cf49dfe
Alla-Nazarenko <alla.nazarenko2022@gmail.com>
2026-04-15 11:51:57 +0200

    hero section added

---
+++ app/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import css from "./HeroSection.module.css";
+
+export default function HeroSection() {
+  return (
+    <div className={css.sectionContainer}>
+      <div className="container">
+        <section className={css.heroSectionInner}>
+          <h1 className={css.mainTitle}>Campers of your dreams</h1>
+          <h2 className={css.subTitile}>
+            You can find everything you want in our catalog
+          </h2>
+          <button className={css.viewNowBtn}>View Now</button>
+        </section>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import Image from "next/image";
 import css from "./page.module.css";
 
 import Header from "./components/Header/Header";
+import HeroSection from "./components/HeroSection/HeroSection";
 
 export default function Home() {
   return (
     <div className={css.page}>
       <main className={css.main}>
         <Header />
+        <HeroSection />
       </main>
     </div>
   );
