b38c2142b710cd680756b43e66e922b97b69fdb3
Alla-Nazarenko <alla.nazarenko2022@gmail.com>
2026-04-15 10:57:13 +0200

    header is ready

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,5 +1,43 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
+import css from "./Header.module.css";
+
 export default function Header() {
-  return <header></header>;
+  const pathname = usePathname();
+
+  return (
+    <header className={css.header}>
+      <div className="container">
+        <div className={css.headerInnerContainer}>
+          <Link href="/">
+            <svg className={css.logoIcon}>
+              <use href="iconsprite.svg#icon-logo" />
+            </svg>
+          </Link>
+          <nav>
+            <ul className={css.headerNav}>
+              <li>
+                <Link className={pathname === "/" ? css.isActive : ""} href="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={
+                    pathname.startsWith("/catalog") ? css.isActive : ""
+                  }
+                  href="/catalog"
+                >
+                  Catalog
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 }
